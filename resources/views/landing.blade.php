56b4da5f1ea47af498d0857574dc80682259246f
minh nguyen <minh06081997@gmail.com>
2026-06-06 09:40:52 +0700

    fix: add cache busting for css and js assets

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -21,10 +21,10 @@
     
     <!-- Stylesheets -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}?v={{ filemtime(public_path('css/style.css')) }}">
     
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}?v={{ filemtime(public_path('js/app.js')) }}" defer></script>
 </head>
 <body>
 
